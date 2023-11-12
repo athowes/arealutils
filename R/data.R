5f7a1a82a4270fd9410a8343dc29ed11c11d1dc1
athowes <adamthowes@gmail.com>
2023-11-12 13:19:31 +0000

    Remove unneeded data

--- R/data.R
+++ R/data.R
@@ -1,19 +1,4 @@
-#' Cote d'Ivoire DHS 2012 Survey Data.
-#'
-#' @format A simple features data frame with 33 rows and 17 variables.
-"ci"
-
 #' Malawi DHS 2015 Survey Data.
 #'
 #' @format A simple features data frame with 28 rows and 17 variables.
 "mw"
-
-#' Tanzania AIS 2012 Survey Data.
-#'
-#' @format A simple features data frame with 159 rows and 17 variables.
-"tz"
-
-#' Zimbabwe DHS 2015 Survey Data.
-#'
-#' @format A simple features data frame with 60 rows and 17 variables.
-"zw"
