573657eeebe142734b368d31b08525ab46966994
athowes <adamthowes@gmail.com>
2023-11-12 13:32:17 +0000

    Correct notation in bym2.hpp

--- src/TMB/bym2.hpp
+++ src/TMB/bym2.hpp
@@ -20,11 +20,11 @@ Type bym2(objective_function<Type>* obj) {
   PARAMETER_VECTOR(u); // Spatial effects
   PARAMETER_VECTOR(w); // Spatial component of spatial effects
   PARAMETER(logit_phi); // 
-  PARAMETER(log_sigma_phi); // Log standard deviation of spatial effects
+  PARAMETER(log_sigma_u); // Log standard deviation of spatial effects
   
   // Transformed parameters block
-  Type sigma_phi(exp(log_sigma_phi));
-  vector<Type> eta(beta_0 + sigma_phi * u);
+  Type sigma_u(exp(log_sigma_u));
+  vector<Type> eta(beta_0 + sigma_u * u);
   vector<Type> rho(invlogit(eta));
   Type phi(invlogit(logit_phi));
   
@@ -33,7 +33,7 @@ Type bym2(objective_function<Type>* obj) {
   nll = Type(0.0);
   
   // Likelihood from priors
-  nll -= dnorm(sigma_phi, Type(0), Type(2.5), true) + log_sigma_phi; // Change of variables
+  nll -= dnorm(sigma_u, Type(0), Type(2.5), true) + log_sigma_u; // Change of variables
   nll -= dnorm(beta_0, Type(-2), Type(1), true); // NB: true puts the likelihood on the log-scale
   
   nll -= log(phi) +  log(1 - phi);  // Change of variables: logit_phi -> phi
@@ -41,9 +41,9 @@ Type bym2(objective_function<Type>* obj) {
   
   // BYM2
   // Constant terms omitted: -0.5 * (n + rank(Q)) * log(2 * M_PI) + 0.5 * log|Q|
-  nll -= -0.5 * n * (2 * log(sigma_phi) + log(1 - phi));  // Normalising constant
-  nll -= -0.5 / (sigma_phi * sigma_phi * (1 - phi)) * (u * u).sum();
-  nll -= sqrt(phi) / (sigma_phi * (1 - phi)) * (u * w).sum();
+  nll -= -0.5 * n * (2 * log(sigma_u) + log(1 - phi));  // Normalising constant
+  nll -= -0.5 / (sigma_u * sigma_u * (1 - phi)) * (u * u).sum();
+  nll -= sqrt(phi) / (sigma_u * (1 - phi)) * (u * w).sum();
   nll -= -0.5 * (w * (Q * w)).sum();
   nll -= -0.5 * phi / (1 - phi) * (w * w).sum();
   
