88be120e394ee942363a4bbeec25f9da0ccce875
athowes <adamthowes@gmail.com>
2023-11-17 11:24:35 +0000

    Add 1, 2, 3, 4 (better names for these?)

--- R/update-naming.R
+++ R/update-naming.R
@@ -6,22 +6,32 @@
 update_naming <- function(df) {
   mutate(
     df,
-    geometry = recode_factor(geometry,
-                             "grid" = "Grid",
-                             "civ" = "Cote d'Ivoire",
-                             "tex" = "Texas"),
-    sim_model = recode_factor(sim_model,
-                              "iid" = "IID",
-                              "icar" = "Besag",
-                              "ik" = "IK"),
-    inf_model = recode_factor(inf_model,
-                              "constant_aghq" = "Constant",
-                              "iid_aghq" = "IID",
-                              "besag_aghq" = "Besag",
-                              "bym2_aghq" = "BYM2",
-                              "fck_aghq" = "FCK",
-                              "ck_aghq" = "CK",
-                              "fik_aghq" = "FIK",
-                              "ik_aghq" = "IK")
+    geometry = recode_factor(
+      geometry,
+      "1" = "1",
+      "2" = "2",
+      "3" = "3",
+      "4" = "4",
+      "grid" = "Grid",
+      "civ" = "Cote d'Ivoire",
+      "tex" = "Texas"
+    ),
+    sim_model = recode_factor(
+      sim_model,
+      "iid" = "IID",
+      "icar" = "Besag",
+      "ik" = "IK"
+    ),
+    inf_model = recode_factor(
+      inf_model,
+      "constant_aghq" = "Constant",
+      "iid_aghq" = "IID",
+      "besag_aghq" = "Besag",
+      "bym2_aghq" = "BYM2",
+      "fck_aghq" = "FCK",
+      "ck_aghq" = "CK",
+      "fik_aghq" = "FIK",
+      "ik_aghq" = "IK"
+    )
   )
 }
